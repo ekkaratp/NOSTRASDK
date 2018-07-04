dbd01068f979371400087653d3520cd31cf9b9b5
Ekkarat Prasongsap <ekkarat.p@geotalent.co.th>
2018-07-04 13:36:20 +0700

    v3.0.4-beta

---
+++ NOSTRASDK.framework/Headers/NOSTRASDK.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NOSTRASDK.
+FOUNDATION_EXPORT double NOSTRASDKVersionNumber;
+
+//! Project version string for NOSTRASDK.
+FOUNDATION_EXPORT const unsigned char NOSTRASDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NOSTRASDK/PublicHeader.h>
+
+
